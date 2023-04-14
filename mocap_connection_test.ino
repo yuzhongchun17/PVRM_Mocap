9a4454341bb1cdbd883fe9b25a2b2fff125de59b
Zhongchun Yu <42987414+yuzhongchun17@users.noreply.github.com>
2023-04-14 18:27:57 -0500

    Add files via upload

---
+++ mocap_connection_test.ino
@@ -0,0 +1,24 @@
+//Mocap Pin
+const int en_mocap_Pin = 13;
+bool isMocapSignal = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  // initialize serial communication
+  Serial.begin(115200);
+  //Setup for mocap
+    pinMode(en_mocap_Pin, INPUT_PULLDOWN);
+    delay(100);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(!digitalRead(en_mocap_Pin)) {}
+  while(digitalRead(en_mocap_Pin)) {
+    isMocapSignal = true;
+  }
+
+  while(isMocapSignal) {
+    Serial.println("Received mocap signals!");
+  }
+}
